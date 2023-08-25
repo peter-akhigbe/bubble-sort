b192807379dccb0105e1eb068aa56c469ec99d23
Peter Akhigbe <akhigbe37@gmail.com>
2023-08-25 16:37:03 +0100

    Update script.rb

--- script.rb
+++ script.rb
@@ -3,15 +3,16 @@ my_array = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
 def bubble_sort(arr)
   loop do
      swapped = false
+      
      (arr.length - 1).times do |index|
       if arr[index] > arr[index + 1]
         arr[index], arr[index + 1] = arr[index + 1], arr[index]
         swapped = true
       end
-    end
-      break if swapped == false
+     end
+      
+     return arr if !swapped
   end
-  arr
 end
 
 p bubble_sort(my_array) # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
